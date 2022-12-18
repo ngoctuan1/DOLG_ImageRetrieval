4240f236ad6e1180b3280a625686212099be8f85
DESKTOP-PCPA7E5\tuann <tuanngoc06102000@gmail.com>
2022-12-18 14:02:16 +0700

    Update

--- prepare_data.sh
+++ prepare_data.sh
@@ -8,4 +8,4 @@ echo "Format data"
 python format_data.py --data_path data
 
 echo "Prepare CSV"
-python prepare_data_csv.py --data_path data --output_path data 
+python prepare_data_csv.py --data_path data --output_path data_files 
--- prepare_data_csv.py
+++ prepare_data_csv.py
@@ -6,7 +6,7 @@ import os
 from tqdm import tqdm
 import json
 
-def get_list_image_to_csv(lst_train, output_path, id2id_encode):
+def get_list_image_to_csv(lst_train, output_path, id2id_encode, drop_minor_class= False):
   csv_content = []
   for img_path in lst_train:
     split_img = img_path.split("/")
@@ -20,13 +20,19 @@ def get_list_image_to_csv(lst_train, output_path, id2id_encode):
     })
 
   df = pd.DataFrame(csv_content)
-  df.to_csv(output_path, index=False)
+  if drop_minor_class == True:
+    del_list = ['cornmarket', 'hertford','oriel','pitt_rivers','worcester']
+    df = df.drop(df[df.id.isin(del_list)].index)
+    df.to_csv(output_path, index=False)
+  else:
+    df.to_csv(output_path, index=False)
   print("DONE")
 def parse_args():
 
   parser = argparse.ArgumentParser()
   parser.add_argument('--data_path', type=str, required=True)
   parser.add_argument('--output_path', type=str, required=True)
+  parser.add_argument('--drop_minor_class', action='store_true')
   args, _ = parser.parse_known_args()
   return args
 
@@ -36,7 +42,7 @@ if __name__ == "__main__":
   lst_class.sort()
 
   id2id_encode = {x.split("/")[-1]:idx for idx, x in enumerate(lst_class)}
-  json.dump(id2id_encode, open(f"{args.data_path}/id2id_encode.json", "w"))
+  json.dump(id2id_encode, open(f"{args.output_path}/id2id_encode.json", "w"))
 
   lst_train, lst_val = [], []
   for class_name in tqdm(lst_class):
@@ -45,6 +51,9 @@ if __name__ == "__main__":
     lst_val.extend(list(map(lambda x: f'{class_name}/{x}', lst_img[-10:])))
 
   os.makedirs(args.output_path, exist_ok=True)
-
-  get_list_image_to_csv(lst_train, f'{args.output_path}/train.csv', id2id_encode)
-  get_list_image_to_csv(lst_val, f'{args.output_path}/val.csv', id2id_encode)
+  if args.drop_minor_class:
+    get_list_image_to_csv(lst_train, f'{args.output_path}/train.csv', id2id_encode, drop_minor_class=True)
+    get_list_image_to_csv(lst_val, f'{args.output_path}/val.csv', id2id_encode, drop_minor_class=True)
+  else:
+    get_list_image_to_csv(lst_train, f'{args.output_path}/train.csv', id2id_encode)
+    get_list_image_to_csv(lst_val, f'{args.output_path}/val.csv', id2id_encode)
